8118ed317d5a37eedfd1ddd43c04b10949b5493c
kuru <kurushetra5@gmail.com>
2018-02-07 13:37:46 +0100

    Initial Commit

---
+++ FireWarWall/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FireWarWall
+//
+//  Created by Kurushetra on 7/2/18.
+//  Copyright © 2018 Kurushetra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
