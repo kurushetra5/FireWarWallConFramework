681fcabcf4b3e19927383b18fd054b46101d8121
kuru <kurushetra5@gmail.com>
2018-04-04 02:30:45 +0200

    los timer se guardan en el Array funciona de momento con uno falta probar con dos procesos

--- FireWarWall/Managers/AppController.swift
+++ FireWarWall/Managers/AppController.swift
@@ -103,10 +103,6 @@ final class AppController:IPLocatorDelegate ,dataBaseDelegate ,ComandsRunerDeleg
             print(result.dataString)
             
         }
-        
-        
-        
-        
     }
     
     
